1569ed3979abb060882871faa73e994d1b295837
shivchawla <shivchawla2001@gmail.com>
2021-08-04 23:48:48 -0600

    Missed code for last check-in

--- src/helper/clock.js
+++ src/helper/clock.js
@@ -2,7 +2,9 @@ import React, {useState} from 'react';
 import { useQuery } from 'react-query';
 import { getClock } from './api';
 import { getCalendar } from './api';
-import { currentISODate, NDaysAgoISODate } from '../utils';
+import { currentISODate, NDaysAgoISODate, dayStartISODate, latestDayStartFromCalendar } from '../utils';
+import { getStorageData } from './store';
+
 
 export function useClock(params={}) {
 	const {isError, data: clock, refetch} = useQuery('clock', getClock, {refetchOnMount: false, ...params});
@@ -20,4 +22,37 @@ export function useCalendar({start = NDaysAgoISODate(5, "YYYY-MM-DD"), end = cur
 	// console.log(query);
 	const {isError, data: calendar, refetch} = useQuery(['calendar', query], () => getCalendar(query), {refetchOnMount: false, ...params});
 	return {calendar, getCalendar: () => refetch().then(r => r.data)}; 
+}
+
+export function useLatestTradingDay() {
+	const [latestTradingDay, setLatestTradingDay] = useState(null);
+	const { getClock} = useClock({enabled: false});
+	const { getCalendar } = useCalendar(null, {enabled: false});
+
+	React.useEffect(() => {
+		const fetchTradingDate = async() => {
+			const td = await getStorageData('latestTradingDay')
+			if(!!td?.datetime && !!td?.lastUpdated) {
+
+				const lastUpdated = td?.lastUpdated;
+
+				//Check last updated time 
+				//What shoudl be the condition
+				setLatestTradingDay(td.datetime);
+
+			} else {
+				const calendar = await getCalendar();
+				const clock = await getClock();
+
+				const latest = clock.is_open ? dayStartISODate() : latestDayStartFromCalendar(calendar, clock);
+				console.log("latestTradingDay");
+				console.log(latest);
+				await setStorageData('latestTradingDay', JSON.stringify({datetime: latest, lastUpdated: clock.timestamp});
+				
+				setLatestTradingDay(latest);
+			}
+		}
+	}, []);
+
+	return latestTradingDay;
 }
